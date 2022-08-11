6e67ed2ac0e2ce08ef2dc827c75130a383faa937
Juanito Tamboong <93064994+JuanitoTamboong@users.noreply.github.com>
2022-08-11 04:18:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ window.onload = function() {
     
     function signUpFunc() {
         var altr = prompt("What's Your Name? ");
-        alert("Hi, " + altr + " can u be my girlfriend?");
+        alert("Hi, " + altr + " alam mo bang bagay ka sakin");
         
     }
     };
